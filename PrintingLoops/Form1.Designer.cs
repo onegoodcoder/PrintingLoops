7cde0a9405d24ce73e9b64f04b560b19a7973203
admin <admin@PC08>
2021-12-18 15:28:42 -0500

    Add project files.

---
+++ PrintingLoops/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace PrintingLoops
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(172, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Show 1, 2, 3, 4, 5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 37);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(172, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Show 5, 4, 3, 2, 1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(172, 379);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PrintingLoops/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintingLoops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//clears the rich text box
+            //print 1, 2, 3, 4, 5
+            //values begin at 1, values increase by 1, last value is 5
+            //int is the data type like -10,0,50, not 1.353
+            int i = 1;//list begins at 1, so type int i=1
+            //i will assukme the values: 1 2 3 4 5
+            //i<=5 because a loop runs, i=1, i=2, i=3, i=4, i=5
+            //when i = 6, 6<=5, so the loop is skipped
+            while (i <= 5)
+            {
+                richTextBox1.Text += $"i={i}\n";
+                i++;//this is needed to grlow the value of i on each cycle of the loop
+
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//clears the rich text box
+            //list values 5  4 3 2 1
+            //lst stats at 5, goes down by 1, endfs on 1
+            int i = 5;//because the list stats at 5, set the value of i to 5
+            //list i=5 i=4 i=3 i=2 i=1, each of the previos makes the loop run
+            //i=0, 0>=1 fails, so loop code do9es not run again
+            while(i>=1)
+            {
+                richTextBox1.Text += $"i={i}\n";
+                i--;//this has the effect of decreasing the value of i on each cycle of the loop
+            }
+        }
+    }
+}
